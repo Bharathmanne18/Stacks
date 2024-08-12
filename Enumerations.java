2947b309a01d82c9d6578b7dbf4f79bc33c1a31f
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-12 20:19:30 +0530

    Update Enumerations.java

--- Enumerations.java
+++ Enumerations.java
@@ -1,8 +1,23 @@
-public class Enumerations{
-  public static void main(String[]args){
-   // Create an Object for Stack
-  // Load person class Objects in it
-  // Iterate it by using Enumeration
-  // print the values by using while Loop
+ package collection;
 
-  }}
+import java.util.Enumeration;
+import java.util.Stack;
+
+public class Enumerations {
+        public static void main(String[]args){
+         // Create an Object for Stack
+        // Load person class Objects in it
+        // Iterate it by using Enumeration
+        // print the values by using while Loop
+         Stack<Person> s=new Stack<>();
+      s.push(new Person("Bharath", 22));
+      s.push(new Person("Ammu", 20));
+      s.push(new Person("Queen", 100));
+     Enumeration<Person> enumeration = s.elements();
+        
+        while(enumeration.hasMoreElements()){
+            System.out.println(enumeration.nextElement());
+      
+        }
+    }
+}
