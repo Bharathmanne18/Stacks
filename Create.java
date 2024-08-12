3acd4e8c26ef2ed6dcc035350cbd3cd8e06d8ab3
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-12 19:32:32 +0530

    Update Create.java

--- Create.java
+++ Create.java
@@ -1,7 +1,18 @@
-public class Create{
-public static void main(String[]args){
-// Create an Object for Stack
-  // Load Values in Stack
-  // Iterate it by usig Iterator and While Loops
-}
-}
+ package collection;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Create {
+    public static void main(String[] args) {
+        Stack<String> s=new Stack<>();
+        s.push("Bharath");
+        s.push("Ammu");
+        s.push("Queen");
+        Iterator<String> iterator = s.iterator();
+        
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+      }
+    }
+        
