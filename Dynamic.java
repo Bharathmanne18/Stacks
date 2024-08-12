2b7f774f79cd17b6af0b0392ecedd4e75783750e
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-12 19:58:21 +0530

    Update Dynamic.java

--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,22 @@
+ package collection;
+import java.util.ListIterator;
+import java.util.Stack;
+
 public class Dynamic{
-public static void main(String[]args){
-// Create an Object for Stack
-  // Load person class Objects in it using push function
-  // Iterate it by using List Iterator
-  // print the values by using while Loop
+    public static void main(String[]args){
+    // Create an Object for Stack
+      // Load person class Objects in it using push function
+      // Iterate it by using List Iterator
+      // print the values by using while Loop
+      Stack<Person> s=new Stack<>();
+      s.push(new Person("Bharath", 22));
+      s.push(new Person("Ammu", 20));
+      s.push(new Person("Queen", 100));
+        ListIterator<Person> iterator = s.listIterator();
+        
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+    }
+    }
 }
-}
+    
