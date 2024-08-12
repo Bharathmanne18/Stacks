9ec85a7394b855e848f5afbadaab7278819caa00
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-12 19:57:48 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,22 @@
+package collection;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name,int age){
+        this.name=name;
+        this.age=age;
+
+    }
+    public String getname(){
+     return name;
+    }
+    public int getage(){
+        return age;
+    }
+    public String toString(){
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+}
+
